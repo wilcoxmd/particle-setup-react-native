60498d49e3a5337a190c2a5c29b83e1b958f0336
Matt Wilcox <mwilcox@lunalights.org>
2018-12-18 20:55:38 -0600

    remove sign in screen header

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -3,13 +3,11 @@ import { View, Text, Button, AsyncStorage } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 export class SignInScreen extends React.Component {
-  static navigationOptions = {
-    title: "Please sign in"
-  };
+  static navigationOptions = { header: null };
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Button title="Super Easy Sign In!" onPress={this._signInAsync} />
       </View>
     );
