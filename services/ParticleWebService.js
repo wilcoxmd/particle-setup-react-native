bed9caa33f2a6fb2d79b8f86b25d2b6e5d62e6cc
Matt Wilcox <mwilcox@lunalights.org>
2019-01-04 12:53:41 -0600

    add function to get device info

--- services/ParticleWebService.js
+++ services/ParticleWebService.js
@@ -1,11 +1,13 @@
 const particleApiEndpoint = "https://api.particle.io/v1";
 import fetch from "react-native-fetch-polyfill";
 
+const particleAPI = "https://api.particle.io/v1";
+
 export default class ParticleWebService {
   static async listCustomerDevices(accessToken) {
     try {
       const deviceList = await fetch(
-        `https://api.particle.io/v1/devices?access_token=${accessToken}`,
+        `${particleAPI}/devices?access_token=${accessToken}`,
         {
           timeout: 5000,
           method: "GET",
@@ -14,6 +16,23 @@ export default class ParticleWebService {
       );
       const deviceListJson = await deviceList.json();
       return deviceListJson;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async getDeviceInfo(deviceId, accessToken) {
+    try {
+      const device = await fetch(
+        `${particleAPI}/devices/${deviceId}?access_token=${accessToken}`,
+        {
+          timeout: 5000,
+          method: "GET",
+          dataType: "JSON"
+        }
+      );
+      const deviceJson = await device.json();
+      return deviceJson;
     } catch (err) {
       console.log(err);
     }
