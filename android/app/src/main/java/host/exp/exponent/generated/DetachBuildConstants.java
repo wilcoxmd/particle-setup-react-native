6af442a60a0a8508136bccf65f25fa81e1d64f8d
Matt Wilcox <mwilcox@lunalights.org>
2019-01-04 15:28:39 -0600

    detach to expokit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp828261c7a4574f65a8fb3a1504494044://192.168.1.172:19000";
+
+}
